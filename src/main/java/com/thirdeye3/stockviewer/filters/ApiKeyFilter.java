c82926b329546a4d2e41728a10b26fb878d05aa2
Kartikey2001nitrr <kartikeythawait123@gmail.com>
2025-12-04 22:51:22 +0530

    updated external api calls

--- src/main/java/com/thirdeye3/stockviewer/filters/ApiKeyFilter.java
+++ src/main/java/com/thirdeye3/stockviewer/filters/ApiKeyFilter.java
@@ -18,17 +18,18 @@ public class ApiKeyFilter extends OncePerRequestFilter {
     @Value("${thirdeye.api.key}")
     private String apiKey;
 
-    @Value("${self.url}")
-    private String selfUrl;
-
     private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        return path.startsWith("/api/statuschecker");
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-
-        String requestUrl = request.getRequestURL().toString();
 
         String requestApiKey = request.getHeader("THIRDEYE-API-KEY");
         if (requestApiKey == null) {
